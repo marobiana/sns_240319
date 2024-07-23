2ea79b107bca63d41fa7e5a652a6339cc8174a0f
DESKTOP-A4BK9EQ\user <test@test.com>
2024-07-23 18:48:57 +0900

    좋아요 토글 기능

---
+++ src/main/java/com/sns/like/bo/LikeBO.java
@@ -0,0 +1,28 @@
+package com.sns.like.bo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sns.like.mapper.LikeMapper;
+
+@Service
+public class LikeBO {
+
+	@Autowired
+	private LikeMapper likeMapper;
+	
+	// input:postId, userId   output:X
+	public void likeToggle(int postId, int userId) {
+		// 조회
+		int count = likeMapper.selectLikeCountByPostIdUserId(postId, userId);
+		
+		// 여부 => 삭제 or 추가
+		if (count > 0) {
+			// 있으면 삭제
+			likeMapper.deleteLikeByPostIdUserId(postId, userId);
+		} else {
+			// 없으면 추가
+			likeMapper.insertLike(postId, userId);
+		}
+	}
+}
